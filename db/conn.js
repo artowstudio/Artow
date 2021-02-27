220d2edefd650d7661e7398e9d138abefd093a67
mujeeb <artowstudio@gmail.com>
2021-02-27 19:01:10 +0530

    modified

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 // Mongodb Connection
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://artow:@221Bsherlocked@cluster0.zexug.mongodb.net/Artow",{useCreateIndex:true,useNewUrlParser:true,useFindAndModify:false,useUnifiedTopology:true}).then(()=>{
+mongoose.connect("mongodb+srv://omar:12345@cluster0.zexug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useCreateIndex:true,useNewUrlParser:true,useFindAndModify:false,useUnifiedTopology:true}).then(()=>{
     console.log("We Are Connected To DataBase")
 }).catch((err)=>{
     console.log(err);
